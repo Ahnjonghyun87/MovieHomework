00eba1d8c8f643d25ba63164966e2597cdf5053b
[Github <helgo1318@gmail.com>
2024-04-28 21:57:59 +0900

    따라해보는데 안되는중

--- movie.js
+++ movie.js
@@ -10,6 +10,30 @@ const options = {
 
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
   .then(response => response.json())
-  .then(response => console.log(response))
+  .then(response => {
+    console.log(response);
+    const movies = response.results;
+    console.log(movies);
+    const moviesElement = document.getElementById('movies');
+    movies.forEach(movie => {
+      const movieCard = document.createElement('div');
+      movieCard.classList.add('movieCard');
+      movieCard.innerHTML = `
+        <div class="movieCard">
+          <img src="${movie.poster_path}"></img>
+          <h2>${movie.title}</h2>
+          <p>${movie.overview}</p>
+          <p>${movie.vote_average}</p>
+        </div>
+      `;
+      moviesElement.appendChild(movieCard);
+    });
+  })
   .catch(err => console.error(err));
+    
+    
+   
+    
+
+
 
